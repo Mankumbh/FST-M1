6bf810020cdb2034274a0c489284aa0b2981bd79
Mankumbh <97943613+Mankumbh@users.noreply.github.com>
2022-02-18 17:21:48 +0530

    Add files via upload

---
+++ Java/Activities/Activity6.java
@@ -0,0 +1,24 @@
+package activities;
+
+public class Activity6 {
+
+    public static void main(String[] args) throws InterruptedException {
+        //There is a plane with max 10 passengers
+        Plane plane = new Plane(10);
+        //Add passengers on the list
+        plane.onboard("John");
+        plane.onboard("Steve");
+        plane.onboard("Anna");
+
+        System.out.println("Plane took off at: " + plane.takeOff());
+
+        System.out.println("People on the plane: " + plane.getPassengers());
+
+        Thread.sleep(5000);
+
+        plane.land();
+        //Plane lands
+        System.out.println("Plane landed at: " + plane.getLastTimeLanded());
+        System.out.println("People on the plane after landing: " + plane.getPassengers());
+    }
+}
