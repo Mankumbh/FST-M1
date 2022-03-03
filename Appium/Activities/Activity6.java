df8a6aa46cc8ee0b0f5ba8e79619ce08319bb38f
Mankumbh <97943613+Mankumbh@users.noreply.github.com>
2022-03-03 15:42:58 +0530

    Add files via upload

---
+++ Appium/Activities/Activity6.java
@@ -0,0 +1,64 @@
+package examples;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+public class Activity6 {
+    AndroidDriver<MobileElement> driver;
+    WebDriverWait wait;
+
+    @BeforeClass
+    public void beforeClass() throws MalformedURLException {
+        // Set the Desired Capabilities
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("deviceId", "11848566");
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("automationName", "UiAutomator2");
+        caps.setCapability("appPackage", "com.android.contacts");
+        caps.setCapability("appActivity", ".activities.PeopleActivity");
+        caps.setCapability("noReset", true);
+
+        // Instantiate Appium Driver
+        URL appServer = new URL("http://0.0.0.0:4723/wd/hub");
+        driver = new AndroidDriver<>(appServer, caps);
+        wait = new WebDriverWait(driver, 5);
+    }
+
+    @Test
+    public void addContact() {
+        driver.get("https://www.training-support.net/selenium/lazy-loading");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.TextView[contains(@text,'Lazy Loading')]")));
+
+        List<MobileElement> numberOfImages = driver.findElementsByXPath("//android.view.View/android.view.View/android.widget.Image");
+        System.out.println("Number of image shown currently: " + numberOfImages.size());
+
+        Assert.assertEquals(numberOfImages.size(), 2);
+
+        driver.findElement(MobileBy.AndroidUIAutomator("UiScrollable(UiSelector().scrollable(true)).scrollTextIntoView(\"helen\")"));
+
+        numberOfImages = driver.findElementsByXPath("//android.view.View/android.view.View/android.widget.Image");
+        System.out.println("Number of image shown currently: " + numberOfImages.size());
+
+        Assert.assertEquals(numberOfImages.size(), 3);
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}
